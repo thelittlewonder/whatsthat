1498116594845e198de59e3b8539c7509caf45ec
Abhishek Sharma <hello.abhishek@outlook.com>
2019-04-04 01:49:29 +0530

    update footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,7 +4,7 @@ class Footer extends Component {
   render() {
     return (
       <div className="footer">
-       <p>Powered by <a href="http://translate.yandex.com/" target="_blank" rel="noopener noreferrer" >Yandex.Translate↗</a>  ·  View on <a href="https://github.com/littlewonder/whatsthat" target="_blank" rel="noopener noreferrer">GitHub↗</a> · By <a href="https://twitter.com/lilwonderspeaks" target="_blank" rel="noopener noreferrer">thelittlewonder</a></p>
+       <p>Powered by <a href="http://translate.yandex.com/" target="_blank" rel="noopener noreferrer" >Yandex.Translate↗</a>&emsp;·&emsp;View on <a href="https://github.com/littlewonder/whatsthat" target="_blank" rel="noopener noreferrer">GitHub↗</a>&emsp;·&emsp;By <a href="https://twitter.com/lilwonderspeaks" target="_blank" rel="noopener noreferrer">thelittlewonder</a></p>
       </div>
     );
   }
